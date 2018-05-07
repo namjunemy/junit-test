68b6f4da38ce01fdd18ba59575ef08b4bea66c19
Namjun Kim <namjunemy@gmail.com>
2018-05-07 18:48:04 +0900

    feat: Calculator subtract test using assertEquals

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,15 +1,23 @@
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class CalculatorTest {
+  Calculator calculator = new Calculator();
 
   @Test
   public void testAdd() {
-    Calculator calculator = new Calculator();
     int sum = calculator.add(1, 3);
 
     assertThat(sum, is(4));
+  }
+
+  @Test
+  public void testSub() {
+    int result = calculator.sub(6, 2);
+
+    assertEquals(result, 4);
   }
 }
